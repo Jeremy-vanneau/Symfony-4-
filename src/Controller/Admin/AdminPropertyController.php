5b4262624bd4bfbfb828d23ec948010f40b09dd9
Yous <jeremyvanneau@gmail.com>
2018-11-19 21:42:57 +0100

    crud des biens fini

--- src/Controller/Admin/AdminPropertyController.php
+++ src/Controller/Admin/AdminPropertyController.php
@@ -1,7 +1,5 @@
 <?php
-
 namespace App\Controller\Admin;
-
 use App\Entity\Property;
 use App\Form\PropertyType;
 use App\Repository\PropertyRepository;
@@ -10,26 +8,21 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
-
 class AdminPropertyController extends AbstractController
 {
     /**
      * @var PropertyRepository
      */
-
     private $repository;
     /**
      * @var ObjectManager
      */
     private $em;
-
     public function __construct(PropertyRepository $repository, ObjectManager $em)
     {
         $this->repository = $repository;
         $this->em = $em;
     }
-
     /**
      * @Route("/admin", name="admin.property.index")
      * @return \Symfony\Component\HttpFoundation\Response
@@ -39,8 +32,6 @@ class AdminPropertyController extends AbstractController
         $properties = $this->repository->findAll();
         return $this->render('admin/property/index.html.twig', compact('properties'));
     }
-
-
     /**
      * @Route ("/admin/property/create" , name="admin.property.new")
      */
@@ -52,6 +43,7 @@ class AdminPropertyController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()){
             $this->em->persist($property);
             $this->em->flush();
+            $this->addFlash('success', 'Votre Bien a était créé.');
             return $this->redirectToRoute('admin.property.index');
         }
         return $this->render('admin/property/new.html.twig', [
@@ -59,7 +51,6 @@ class AdminPropertyController extends AbstractController
             'form' => $form->createView()
         ]);
     }
-
     /**
      * @Route ("/admin/property/{id}", name="admin.property.edit", methods="GET|POST")
      * @param Property $property
@@ -72,6 +63,7 @@ class AdminPropertyController extends AbstractController
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()){
             $this->em->flush();
+            $this->addFlash('success', 'Votre Bien a était modifié');
             return $this->redirectToRoute('admin.property.index');
         }
         return $this->render('admin/property/edit.html.twig', [
@@ -79,19 +71,17 @@ class AdminPropertyController extends AbstractController
             'form' => $form->createView()
         ]);
     }
-
     /**
      * @Route ("/admin/property/{id}", name="admin.property.delete", methods="DELETE")
      * @param Property $property
      * @return \Symfony\Component\HttpFoundation\RedirectResponse
      */
-
     public function delete(Property $property, Request $request){
         if ($this->isCsrfTokenValid('delete' . $property->getId(), $request->get('_token'))){
             $this->em->remove($property);
             $this->em->flush();
+            $this->addFlash('success', 'Votre Bien a était supprimé.');
         }
         return $this->redirectToRoute('admin.property.index');
     }
-
 }
